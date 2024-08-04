10a134fcd6ba414bd809be69e46cfe4c2a81f07e
robin <robinhahn99@outlook.de>
2024-08-04 16:24:43 +0200

    prettier now

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {RegisterService} from "../services/register.service";
 import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -17,21 +18,29 @@ export class RegisterComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private registerService: RegisterService) {}
+  constructor(private registerService: RegisterService, private router: Router) {}
 
   onSubmit() {
-    this.registerService.register(this.username, this.password)
-      .subscribe({
-        next: response => {
-          alert('Registrierung erfolgreich!');
-        },
-        error: error => {
-          alert('Fehler bei der Registrierung.');
-        },
-        complete: () => {
-          console.info('Registrierungsprozess abgeschlossen.');
-        }
-      });
+
+
+
+      this.registerService.register(this.username, this.password)
+        .subscribe({
+          next: response => {
+            alert('Registrierung erfolgreich!');
+          },
+          error: error => {
+            alert('Fehler bei der Registrierung.');
+          },
+          complete: () => {
+            console.info('Registrierungsprozess abgeschlossen.');
+          }
+        });
+
+  }
+
+  navLogin(){
+    this.router.navigate(['login'])
   }
 
 }
