5f01b64b59b994d353120e86be2f27cf021026ec
Linda <fatlinda.islami@mni.thm.de>
2024-09-04 21:38:41 +0200

    navbar fertig hehehehehe

--- frontend/src/app/User/player-profile/player-profile.component.ts
+++ frontend/src/app/User/player-profile/player-profile.component.ts
@@ -3,6 +3,9 @@ import {Router} from "@angular/router";
 import {OptionsComponent} from "./options/options.component";
 import {PlayerContentComponent} from "./player-content/player-content.component";
 import {PlayerPasswordChangeComponent} from "./player-password-change/player-password-change.component";
+import {LogoutService} from "../../Auth/logout/services/logout.service";
+import {SocketService} from "../../Socket/socket.service";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-player-profile',
@@ -17,6 +20,24 @@ import {PlayerPasswordChangeComponent} from "./player-password-change/player-pas
 })
 export class PlayerProfileComponent {
 
+  constructor(
+    private router: Router,
+    private logOut: LogoutService,
+    private socketService: SocketService,
+    public userService: UserService
+  ) {}
+
+  back(){
+    this.router.navigate(['/']);
+  }
+
+  logout(){
+    this.logOut.logout();
+  }
+
+  navProfile(){
+    this.router.navigate(['/profile'])
+  }
 
   activeTab: string = 'general';
 
@@ -25,7 +46,5 @@ export class PlayerProfileComponent {
   }
 
 
-  constructor(private router: Router) {
-  }
 
 }
