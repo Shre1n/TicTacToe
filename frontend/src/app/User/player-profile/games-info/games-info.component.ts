17b357cd5830abeefd0aab84d7f619e57887c29f
Linda <fatlinda.islami@mni.thm.de>
2024-09-08 22:39:31 +0200

    add unversioned Files

---
+++ frontend/src/app/User/player-profile/games-info/games-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './games-info.component.html',
+  styleUrl: './games-info.component.css'
+})
+export class GamesInfoComponent {
+
+}
