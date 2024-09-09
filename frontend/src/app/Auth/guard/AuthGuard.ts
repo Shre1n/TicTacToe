04fd68a40f49e465f380d452cd2f098a6335eecf
Ramon <ramon.biehl@mni.thm.de>
2024-09-09 12:45:59 +0200

    fixed auth

--- frontend/src/app/Auth/guard/AuthGuard.ts
+++ frontend/src/app/Auth/guard/AuthGuard.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {CanActivate, Router} from '@angular/router';
 import { UserService } from '../../User/user.service';
 import { catchError, map, of } from 'rxjs';
+import { UserDto } from '../../User/interfaces/userDto';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ export class AuthGuard implements CanActivate {
 
   canActivate() {
     return this.userService.isAuthenticated().pipe(
-      map((response: Response) => {
-        if (response.ok)
+      map((response: UserDto) => {
+        if (response)
           return true;
         this.router.navigate(['/unauthorized']);
         return false;
