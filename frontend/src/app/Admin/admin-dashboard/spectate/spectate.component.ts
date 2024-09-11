e0c2133df763c9e1afa90716ddbfe77fb9add986
Ramon <ramon.biehl@mni.thm.de>
2024-09-11 13:29:50 +0200

    added spectate component

---
+++ frontend/src/app/Admin/admin-dashboard/spectate/spectate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spectate',
+  standalone: true,
+  imports: [],
+  templateUrl: './spectate.component.html',
+  styleUrl: './spectate.component.css'
+})
+export class SpectateComponent {
+
+}
